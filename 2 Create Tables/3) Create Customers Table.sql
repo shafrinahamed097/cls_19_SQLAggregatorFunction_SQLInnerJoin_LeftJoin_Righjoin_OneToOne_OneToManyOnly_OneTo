17a2b0187e664245c27c360fed100fb4b7a404bf
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-06-24 21:40:32 +0600

    Edit Customers Table.sql

--- 2 Create Tables/3) Create Customers Table.sql
+++ 2 Create Tables/3) Create Customers Table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `customers` (
-    `id` bigint(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(50) NOT NULL,
     `email` VARCHAR(50) NOT NULL,
     `mobile` VARCHAR(50) NOT NULL,
-    `user_id` bigint(20) NOT NULL,
+    `user_id` bigint(20) UNSIGNED NOT NULL,
     `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    `Updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+    `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
     FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
 )
