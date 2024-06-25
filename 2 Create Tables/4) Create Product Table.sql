86296b7196f2ca2392148ea00196ba503bd2ae40
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-06-25 12:22:05 +0600

    Genera Product table sql

---
+++ 2 Create Tables/4) Create Product Table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `products`(
+    `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `user_id` bigint(20) UNSIGNED NOT NULL,
+    `category_id` bigint(20) UNSIGNED NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `price` VARCHAR(50) NOT NULL,
+    `unit` VARCHAR(50) NOT NULL,
+    `img_url` VARCHAR(100) NOT NULL,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (`category_id`) REFERENCES `categories`(`id) ON DELETE RESTRICT ON UPDATE CASCADE
+)
